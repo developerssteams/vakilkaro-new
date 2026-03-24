61ff6f408b7348f6ae16e1f3903145e6c67bc1fc
Developers <developerss.teams@gmail.com>
2026-03-24 17:17:32 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/verified" element={<Verified />} />
+        <Route path="/Verified" element={<Verified />} />
       </Routes>
       <Footer />
     </Router>
