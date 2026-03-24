ece9397a7967511bca9acb620c35a849e18313f1
Developers <developerss.teams@gmail.com>
2026-03-24 17:23:04 +0530

    Initial commit

--- src/pages/Verified.jsx
+++ src/pages/Verified.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import HeaderSection from "../components/HeaderSection";
 
-import WhyVakilkaro from "../components/WhyVakilkaro";
-import Timeline from "../components/Timeline";
-// import ClientsSection from "../components/ClientsSection";
-// import Testimonials from "../components/Testimonials";
-import PopularSearches from "../components/PopularSearches";
 import TawkTo from "../components/TawkTo";
-import StorySection from "../components/StorySection";
-// import ServiceSlider from "../components/ServiceSlider";
-import VerifiedServices from "../components/VerifiedServices";
-import LegalRepresentation from "../components/LegalRepresentation";
-import Partner from "../components/Partner";
-import Journey from "../components/Journey";
+
 import Story from "../components/Story";
-import CallToAction from "../components/CallToAction";
-import ExpertSlider from "../components/LeadershipSlider";
-import Advocates from "../components/Advocates";
-import VakilKaroWay from "../components/VakilKaroWay";
+
 import Testimonials from "../components/Testimonials";
 import ContactSection from "../components/ContactSection";
 import Faq from "../components/Faq";
@@ -28,28 +14,16 @@ function Verified() {
     return (
         <>
 
-            {/* <VerifiedServices /> */}
-            {/* <LegalRepresentation /> */}
-            {/* <Partner /> */}
-            {/* <Journey /> */}
             <Navbar_Section />
             <Query_Card />
             <Trademark_Registration />
-            {/* <WhyVakilkaro /> */}
-            {/* <CallToAction /> */}
+    
             <Story />
-            {/* <StorySection /> */}
-            {/* <ServiceSlider /> */}
-            {/* <ExpertSlider /> */}
-            {/* <Advocates /> */}
-            {/* <VakilKaroWay /> */}
+        
             <Testimonials />
             <ContactSection />
             <Faq />
-            {/* <Timeline /> */}
-            {/* <ClientsSection /> */}
-            {/* <PopularSearches /> */}
-            {/* <BlogSection /> */}
+       
             <TawkTo />
 
 
