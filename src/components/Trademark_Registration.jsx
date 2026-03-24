2ae7eeef140ca5cb8ece8362810806f47363d1a0
Developers <developerss.teams@gmail.com>
2026-03-24 17:43:08 +0530

    Initial commit

--- src/components/Trademark_Registration.jsx
+++ src/components/Trademark_Registration.jsx
@@ -5,34 +5,34 @@ import ctaImg from "../assets/call_to_action.png";
 const CallToAction = () => {
     return (
 
-     <section className="trademark_registration-cta-section">
-    <div className="trademark_registration-cta-inner">
+        <section className="trademark_registration-cta-section">
+            <div className="trademark_registration-cta-inner">
 
-        <div className="trademark_registration-cta-content">
-            <h2>
-                Expert Advice,<br />
-                <span>Just a Call Away</span>
-            </h2>
+                <div className="trademark_registration-cta-content">
+                    <h2>
+                        Expert Advice,<br />
+                        <span>Just a Call Away</span>
+                    </h2>
 
-            <p>
-                Get immediate answers to your legal questions from our top-tier team.
-            </p>
+                    <p>
+                        Get immediate answers to your legal questions from our top-tier team.
+                    </p>
 
-            <div className="trademark_registration-cta-buttons">
-                <button className="trademark_registration-btn primary">Call Us</button>
+                    <div className="trademark_registration-cta-buttons">
+                        <button className="trademark_registration-btn primary">Call Us</button>
 
-                <button className="trademark_registration-btn outline">
-                    Whatsapp <FiArrowUpRight />
-                </button>
+                        <button className="trademark_registration-btn outline">
+                            Whatsapp <FiArrowUpRight />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="trademark_registration-cta-image">
+                    <img src={ctaImg} alt="Expert Support" />
+                </div>
+
             </div>
-        </div>
-
-        <div className="trademark_registration-cta-image">
-            <img src={ctaImg} alt="Expert Support" />
-        </div>
-
-    </div>
-</section>
+        </section>
     );
 };
 
