f608c9569b70855934ade7487d0cff0ffd6268cf
Developers <developerss.teams@gmail.com>
2026-03-25 10:36:25 +0530

    route solve

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Verified" element={<Verified />} />
+        <Route path="Verified" element={<Verified />} />
       </Routes>
       <Footer />
     </Router>
